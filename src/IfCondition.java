8fce77e615951f3f9cf15ce147a794f6bf5adbc3
Reperi-toxi <gigicho.ch@gmail.com>
2024-12-31 20:47:45 +0400

    Update IfCondition.java

--- src/IfCondition.java
+++ src/IfCondition.java
@@ -7,37 +7,6 @@ public class IfCondition {
     }
 
     public boolean evaluateCondition(Map<String, Integer> variables) {
-        // Evaluate condition such as 'sum > 20'
-        if (condition.contains(">")) {
-            return evaluateComparisonCondition(variables, ">");
-        }
-        // Evaluate condition such as 'sum < 20'
-        else if (condition.contains("<")) {
-            return evaluateComparisonCondition(variables, "<");
-        }
-        // Evaluate condition such as 'sum == 20'
-        else if (condition.contains("==")) {
-            return evaluateComparisonCondition(variables, "==");
-        }
-        return false;
-    }
-
-    private boolean evaluateComparisonCondition(Map<String, Integer> variables, String operator) {
-        String[] conditionParts = condition.split("\\s" +operator);
-        if (conditionParts.length > 1) {
-            String left = conditionParts[0].trim();
-            String right = conditionParts[1].trim();
-
-            int leftValue = variables.getOrDefault(left, 0);
-            int rightValue = Integer.parseInt(right);
-
-            return switch (operator) {
-                case ">" -> leftValue > rightValue;
-                case "<" -> leftValue < rightValue;
-                case "==" -> leftValue == rightValue;
-                default -> false;
-            };
-        }
-        return false;
+        return BooleanEvaluator.evaluate(condition, variables);
     }
 }
